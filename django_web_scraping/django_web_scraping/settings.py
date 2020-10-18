74aca5d45b676a08bb1efbc184b874e7b3c50b2f
AlexDannn <46036358+AlexDannn@users.noreply.github.com>
2020-10-18 23:22:23 +0300

    Update settings.py

--- django_web_scraping/django_web_scraping/settings.py
+++ django_web_scraping/django_web_scraping/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('SECRET_KEY') 
-SECRET_KEY = '_i8o-(q)vhx*o4%(*bxhwu)=tb!m56+_s_t*jvbsciqi46h=w0'
+SECRET_KEY = os.environ.get('SECRET_KEY') 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 DEBUG = False
